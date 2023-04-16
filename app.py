360d3150f8437560b2906ee9e8be3c7c189050f7
Léo Recouvreux <leo.recouvreux@gmail.com>
2023-04-16 22:52:15 +0200

    timer ok, set interval to correct captcha

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 import os
+import schedule
+import time
+import random
 
 from flask import Flask, jsonify
 from tgtg import TgtgClient
@@ -12,14 +15,10 @@ app = Flask(__name__)
 load_dotenv()
 
 # Twilio configuration
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN', None)
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER', None)
-MY_PHONE_NUMBER = os.getenv('MY_PHONE_NUMBER', None)
-print(TWILIO_ACCOUNT_SID)
-print(TWILIO_AUTH_TOKEN)
-print(TWILIO_PHONE_NUMBER)
-print(MY_PHONE_NUMBER)
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+MY_PHONE_NUMBER = os.getenv('MY_PHONE_NUMBER')
 
 twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
@@ -28,17 +27,21 @@ TGTG_ACCESS_TOKEN = os.getenv('TGTG_ACCESS_TOKEN')
 TGTG_REFRESH_TOKEN = os.getenv('TGTG_REFRESH_TOKEN')
 TGTG_USER_ID = os.getenv('TGTG_USER_ID')
 TGTG_COOKIE = os.getenv('TGTG_COOKIE')
-print(TGTG_ACCESS_TOKEN, "TG_ACCESS_TOKEN")
-print(TGTG_REFRESH_TOKEN, "TG_REFRESH_TOKEN")
-print(TGTG_USER_ID, "TG_USER_ID")
-print(TGTG_COOKIE, "TG_COOKIE")
 
 client = TgtgClient(
-    access_token="e30.eyJzdWIiOiIyMTAwOTQ4NyIsImV4cCI6MTY4MTgzNjMyMCwidCI6IjRlNGN4RlNKUnBHOU5kZWM5aUQzblE6MDoxIn0.YESpTuEmBxFFcbsLjM0pG741kom3r0k7X2nnOpV6ocA",
-    refresh_token="e30.eyJzdWIiOiIyMTAwOTQ4NyIsImV4cCI6MTcxMzI4NTkyMCwidCI6IkJSeHl6dGZqUzZhTTlXLXVoTmlPS1E6MDowIn0.uyJIopN44-0NzW3VqrFLZHuhe39tDlvYl5mJ4Vnca7E",
-    user_id="21009487",
-    cookie="datadome=2qjkq3g2XLtsWBe7zB71tB_2OmwS67trJ0tDNZi7T1LwN~s7Q8-4XM9-Z_bEy39Xxs1qwUsBhj-I4qsuB8Ntef_UG54AeyltQYAk4kbAKPb~XbhTQMV6-0Niu87m3Qrb"
+    access_token=TGTG_ACCESS_TOKEN,
+    refresh_token=TGTG_REFRESH_TOKEN,
+    user_id=TGTG_USER_ID,
+    cookie=TGTG_COOKIE
 )
+
+def reschedule():
+    delay = random.randint(0, 600)  # Générer un nombre aléatoire entre 0 et 600 secondes (10 minutes)
+    minutes, seconds = divmod(delay, 60)  # Convertir les secondes en minutes et secondes
+    print(f"Next check in {minutes} minutes and {seconds} seconds.")
+    schedule.every(delay).seconds.do(
+        check_items_and_send_sms
+    )  # Planifier le prochain check avec le délai en secondes
 
 
 def send_sms(message):
@@ -58,34 +61,47 @@ def is_access_token_valid():
         client.get_items()
         return True
     except TgtgAPIError as e:
-        if e.status_code == 401:  # Unauthorized
-            return False
-        else:
-            raise e
+        print(f"Error checking access token: {e}")
+        # if e. == 401:  # Unauthorized
+        #     return False
+        # else:
+        #     raise e
     except Exception as e:
         print(f"Error checking access token: {e}")
         return False
 
 
-@app.route('/')
-def home():
+def check_items_and_send_sms():
     if not is_access_token_valid():
         send_sms("Access token is not valid. Please refresh the token.")
-        return jsonify({"error": "Access token is not valid"}), 401
+        return
 
     try:
         items = client.get_items()
         for item in items:
+            time.sleep(5)  # Ajout d'un intervalle de 5 secondes entre chaque appel d'item
             if item["items_available"] > 0:
                 print('available')
                 send_sms(f"{item['display_name']} is available! Go get it! :)")
             else:
                 print('not available')
-        return jsonify(items)
     except Exception as e:
         print(f"Error fetching items: {e}")
-        return jsonify({"error": "Error fetching items"}), 500
+
+    reschedule()
+
+
+
+@app.route('/')
+def home():
+    check_items_and_send_sms()
+    return jsonify({"message": "Check executed"}), 200
 
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    schedule.every(1).minutes.do(check_items_and_send_sms)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        app.run(port=5001)
