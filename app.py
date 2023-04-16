fbb53f352409986dad446dac9eee2e080fb5efee
Léo Recouvreux <leo.recouvreux@gmail.com>
2023-04-16 23:53:31 +0200

    timer ok, add thread process handle

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
+import threading
+
 from dotenv import load_dotenv
 import os
-import schedule
-import time
 import random
-
+import time
 from flask import Flask, jsonify
 from pushbullet import Pushbullet
 from tgtg import TgtgClient
 from tgtg.exceptions import TgtgAPIError, TgtgLoginError
-from twilio.rest import Client
 
 app = Flask(__name__)
 
 # load environment variables from .env file
 load_dotenv()
-
-# Twilio configuration
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-MY_PHONE_NUMBER = os.getenv('MY_PHONE_NUMBER')
-
-twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # TGTG client configuration
 TGTG_ACCESS_TOKEN = os.getenv('TGTG_ACCESS_TOKEN')
@@ -41,44 +32,6 @@ PUSHBULLET_ACCESS_TOKEN = os.getenv('PUSHBULLET_ACCESS_TOKEN')
 pb = Pushbullet(PUSHBULLET_ACCESS_TOKEN)
 
 
-def get_next_run_time():
-    if not schedule.get_jobs():
-        return None
-
-    next_run_time = min(job.next_run for job in schedule.get_jobs())
-    return next_run_time
-
-
-def display_next_schedule():
-    next_check = get_next_run_time()
-    if next_check:
-        print(f"Next check: {next_check}")
-    else:
-        print("No scheduled jobs.")
-
-
-def reschedule():
-    delay = random.randint(0, 600)  # Générer un nombre aléatoire entre 0 et 600 secondes (10 minutes)
-    minutes, seconds = divmod(delay, 60)  # Convertir les secondes en minutes et secondes
-    print(f"Next check in {minutes} minutes and {seconds} seconds.")
-    schedule.every(5).seconds.do(
-        check_items_and_send_sms
-    )  # Planifier le prochain check avec le délai en secondes
-    print("Checking items...")
-
-
-def send_sms(message):
-    try:
-        message = twilio_client.messages.create(
-            body=message,
-            from_=TWILIO_PHONE_NUMBER,
-            to=MY_PHONE_NUMBER
-        )
-        print(f"SMS sent: {message.sid}")
-    except Exception as e:
-        print(f"Error sending SMS: {e}")
-
-
 def send_bullet(title, message):
     try:
         push = pb.push_note(title, message)
@@ -93,10 +46,6 @@ def is_access_token_valid():
         return True
     except TgtgAPIError as e:
         print(f"Error checking access token: {e}")
-        # if e. == 401:  # Unauthorized
-        #     return False
-        # else:
-        #     raise e
     except Exception as e:
         print(f"Error checking access token: {e}")
         return False
@@ -104,7 +53,6 @@ def is_access_token_valid():
 
 def check_items_and_send_sms():
     if not is_access_token_valid():
-        # send_sms("Access token is not valid. Please refresh the token.")
         send_bullet("ERROR: ", "Access token is not valid. Please refresh the token.")
         return
 
@@ -122,7 +70,6 @@ def check_items_and_send_sms():
 
         if available_items:
             message = "\n" + "\n".join(available_items)
-            # send_sms(message)
             send_bullet("Items available", message)
         else:
             print("No items available")
@@ -130,19 +77,35 @@ def check_items_and_send_sms():
     except Exception as e:
         print(f"Error fetching items: {e}")
 
-    reschedule()
+
+def print_remaining_time(start_time, delay):
+    remaining_time = int(delay - (time.time() - start_time))
+    if remaining_time > 0:
+        print(f"Temps restant avant le prochain check: {remaining_time} secondes")
+        threading.Timer(10, print_remaining_time, [start_time, delay]).start()
+
+
+def schedule_check():
+    delay = random.randint(60, 433)  # Générer un nombre aléatoire entre 60 et 120 secondes (1 à 2 minutes)
+    print(f"Next check in {delay} seconds.")
+    print_remaining_time(time.time(), delay)
+    time.sleep(delay)
+    check_items_and_send_sms()
+    schedule_check()
+
+
+def start_schedule_check_in_thread():
+    print("Starting scheduled checks")
+    check_thread = threading.Thread(target=schedule_check)
+    check_thread.start()
 
 
 @app.route('/')
 def home():
-    check_items_and_send_sms()
-    return jsonify({"message": "Check executed"}), 200
+    return jsonify({"message": "Server is running"}), 200
 
 
 if __name__ == '__main__':
-    # schedule.every(1).minutes.do(check_items_and_send_sms)
-    reschedule()
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    print("Starting scheduled checks")
+    start_schedule_check_in_thread()
+    app.run(port=5001)
