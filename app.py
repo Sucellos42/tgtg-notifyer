a800065088ea3cd6dbcbe0dff3c9e60b3498039c
Léo Recouvreux <leo.recouvreux@gmail.com>
2023-04-16 23:39:06 +0200

    timer not ok, replay

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 import random
 
 from flask import Flask, jsonify
+from pushbullet import Pushbullet
 from tgtg import TgtgClient
 from tgtg.exceptions import TgtgAPIError, TgtgLoginError
 from twilio.rest import Client
@@ -35,13 +36,35 @@ client = TgtgClient(
     cookie=TGTG_COOKIE
 )
 
+# Pushbullet configuration
+PUSHBULLET_ACCESS_TOKEN = os.getenv('PUSHBULLET_ACCESS_TOKEN')
+pb = Pushbullet(PUSHBULLET_ACCESS_TOKEN)
+
+
+def get_next_run_time():
+    if not schedule.get_jobs():
+        return None
+
+    next_run_time = min(job.next_run for job in schedule.get_jobs())
+    return next_run_time
+
+
+def display_next_schedule():
+    next_check = get_next_run_time()
+    if next_check:
+        print(f"Next check: {next_check}")
+    else:
+        print("No scheduled jobs.")
+
+
 def reschedule():
     delay = random.randint(0, 600)  # Générer un nombre aléatoire entre 0 et 600 secondes (10 minutes)
     minutes, seconds = divmod(delay, 60)  # Convertir les secondes en minutes et secondes
     print(f"Next check in {minutes} minutes and {seconds} seconds.")
-    schedule.every(delay).seconds.do(
+    schedule.every(5).seconds.do(
         check_items_and_send_sms
     )  # Planifier le prochain check avec le délai en secondes
+    print("Checking items...")
 
 
 def send_sms(message):
@@ -54,6 +77,14 @@ def send_sms(message):
         print(f"SMS sent: {message.sid}")
     except Exception as e:
         print(f"Error sending SMS: {e}")
+
+
+def send_bullet(title, message):
+    try:
+        push = pb.push_note(title, message)
+        print(f"Notification sent: {push}")
+    except Exception as e:
+        print(f"Error sending Pushbullet notification: {e}")
 
 
 def is_access_token_valid():
@@ -73,23 +104,33 @@ def is_access_token_valid():
 
 def check_items_and_send_sms():
     if not is_access_token_valid():
-        send_sms("Access token is not valid. Please refresh the token.")
+        # send_sms("Access token is not valid. Please refresh the token.")
+        send_bullet("ERROR: ", "Access token is not valid. Please refresh the token.")
         return
 
     try:
         items = client.get_items()
+        available_items = []
+
         for item in items:
             time.sleep(5)  # Ajout d'un intervalle de 5 secondes entre chaque appel d'item
             if item["items_available"] > 0:
-                print('available')
-                send_sms(f"{item['display_name']} is available! Go get it! :)")
+                print(f"{item['display_name']} is available")
+                available_items.append(item['display_name'])
             else:
-                print('not available')
+                print(f"{item['display_name']} is not available")
+
+        if available_items:
+            message = "\n" + "\n".join(available_items)
+            # send_sms(message)
+            send_bullet("Items available", message)
+        else:
+            print("No items available")
+
     except Exception as e:
         print(f"Error fetching items: {e}")
 
     reschedule()
-
 
 
 @app.route('/')
@@ -99,9 +140,9 @@ def home():
 
 
 if __name__ == '__main__':
-    schedule.every(1).minutes.do(check_items_and_send_sms)
+    # schedule.every(1).minutes.do(check_items_and_send_sms)
+    reschedule()
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-        app.run(port=5001)
